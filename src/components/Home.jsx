3d07c782b65de64b1fe32a5b70b743f63a48f955
yueqin <momo>
2018-08-01 15:59:32 +0800

    test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -64,7 +64,7 @@ class Home extends Component {
                 <div className="all">
                     <div id="containter">
                         <div className="bg">
-                            <img src={home} alt="图片"/>
+                            <img src={home} alt="标识"/>
                         </div>
                         <div className="loginBox">
                             <div className="formBorder">
